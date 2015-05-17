de12c06fe3ef0588e8e421b511dcdf85a618399d
Generalbelly <shimmennobuyoshi@ShimmenshinoAir.lan>
2015-05-18 00:25:24 +0900

    Update

---
+++ OnTheMap/UIColorExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColorExtension.swift
+//  OnTheMap
+//
+//  Created by ShimmenNobuyoshi on 2015/05/17.
+//  Copyright (c) 2015年 Shimmen Nobuyoshi. All rights reserved.
+//
+
+import Foundation
---
+++ OnTheMap/UdacityStudent.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityStudent.swift
+//  OnTheMap
+//
+//  Created by ShimmenNobuyoshi on 2015/05/13.
+//  Copyright (c) 2015年 Shimmen Nobuyoshi. All rights reserved.
+//
+
+import Foundation
